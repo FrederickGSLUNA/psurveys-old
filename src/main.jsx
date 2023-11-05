3e649cdc963a1c0aa87dc506e75fc7cee4954fcf
AldairGM <101372457+AldariGM@users.noreply.github.com>
2023-11-05 00:48:56 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-
+import { registerServiceWorker } from './utilities/main'; // Importar la función registerServiceWorker
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
 )
+
+registerServiceWorker(); // Registrar el Service Worker
