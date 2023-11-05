50f697509d713fba493f067842571cf5b14ec099
Javier Cervantes Carmona <cervantescarmonaj@gmail.com>
2023-11-04 22:45:29 -0600

    Nuevo

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,10 @@ import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { register } from './utilities/main'; // Importar la función register
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
 )
-register(); // Registrar el Service Worker
